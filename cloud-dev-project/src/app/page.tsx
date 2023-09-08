7e1a47523c9352cf8e384e54d895563dfc95518d
Tin Wongcharoo <37769054+BlueFish01@users.noreply.github.com>
2023-09-08 10:05:14 +0700

    add COLORS constants

--- cloud-dev-project/src/app/page.tsx
+++ cloud-dev-project/src/app/page.tsx
@@ -1,11 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { COLORS } from '@/constants';
 
 function page() {
   return (
     <div>
       <h1>font here!!</h1>
-      <FontAwesomeIcon icon={faEnvelope} />
+      <FontAwesomeIcon icon={faEnvelope} style={{color: COLORS.PRIMARY}}/>
     </div>
   );
 }
