b8ad85e9fc90575976bb52de9285cd4e0d0e7c9c
Tin Wongcharoo <37769054+BlueFish01@users.noreply.github.com>
2023-09-08 09:27:05 +0700

    add fontawsome to the project

--- cloud-dev-project/src/app/page.tsx
+++ cloud-dev-project/src/app/page.tsx
@@ -1,8 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 function page() {
-  
-  return(
-    <h1>font here!!</h1>
+  return (
+    <div>
+      <h1>font here!!</h1>
+      <FontAwesomeIcon icon={faEnvelope} />
+    </div>
   );
 }
 
-export default page
+export default page;
