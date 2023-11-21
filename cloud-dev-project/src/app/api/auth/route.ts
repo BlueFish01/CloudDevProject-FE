fff612d77ab1e6ab4813427b93fd778f4286ed35
Tin Wongcharoo <Tinn.Wongcharoo@gmail.com>
2023-11-22 04:43:47 +0700

    fix api auth

--- cloud-dev-project/src/app/api/auth/route.ts
+++ cloud-dev-project/src/app/api/auth/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     const authToken = request.cookies.get("authToken");
-    return new Response(JSON.stringify({ ...authToken }));
+    console.log(authToken);
+    return NextResponse.json({ ...authToken });
 }
